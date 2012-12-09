16c645aa3b97424abc54d0546ac616da6a430b2e
John Foley <jfoley@cs.umass.edu>
2012-12-09 14:03:06 -0500

    Multiple Target test :(

---
+++ ex/multarget/Test.java
@@ -0,0 +1,12 @@
+class Test {
+  public static void main(String[] args) {
+    Helper h = new Helper();
+    System.out.println(h);
+  }
+}
+
+class Helper {
+  public String toString() {
+    return "Hello World!";
+  }
+}
